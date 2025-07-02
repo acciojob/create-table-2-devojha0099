29d0a5f9fa7a1a0616adbb10bff9db866e6cf0c4
Devendra Kumar Ojha <devojha0099@gmail.com>
2025-07-02 10:57:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 function createTable() {
-    //Write your code here
+    let rows = prompt("Number of Rows: ");
+	let columns = prompt("Number of Columns: ");
+
+	rows = parseInt(rows);
+	columns = parseInt(columns);
+
+	if(isNaN(rows) || isNaN(columns) || rows <=0 || columns <=0){
+		alert("Please Enter a valid Number");
+		return;
+	}
+
+	const table = document.getElementById("myTable");
+	table.innerHTML = '';
+
+
+	for(let i=0;i<rows; i++){
+		const tr = document.createElement("tr");
+		for(let j=0;j<columns; j++){
+			const td = document.createElement("td");
+			td.textContent = `R${i+1} C${j+1}`;
+			tr.appendChild(td);
+		}
+		table.appendChild(tr);
+	}
+	
   
 }
+
+
